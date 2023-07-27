1d42575d128a23a678ec04e16ad5290d3a895b6c
Ahmed Hbaieb <hbaieb.ahmed@hotmail.fr>
2023-07-27 20:43:27 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#from streamlit_chat import message as st_message
+from streamlit_chat import message as st_message
 from transformers import BlenderbotTokenizer
 from transformers import BlenderbotForConditionalGeneration
 
